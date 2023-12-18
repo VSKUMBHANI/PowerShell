af8b12e0393f19ca947b004131fbbb0c5452f76b
Vijay Kumbhani <102287173+VSKUMBHANI@users.noreply.github.com>
2023-12-18 10:25:53 +0530

    Add files via upload

---
+++ Environment Variable/ExportRegEnvVar.ps1
@@ -0,0 +1,40 @@
+﻿# Export registry of Environment Variables...
+
+$SystemVariablePath = "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+$UserVariablePath = "HKCU\Environment"
+$ExportDirPath = "C:\Exports"
+
+If(!(Test-Path $ExportDirPath))
+{
+    mkdir $ExportDirPath
+}
+
+Write-Host "Exporting the System Variables..."
+$DateTime = Get-Date -Format ddMMyyyy_hhmmsstt
+
+If(!(Test-Path $ExportDirPath\SystemVariable.reg))
+{
+    Invoke-Command {reg export $SystemVariablePath $ExportDirPath\SystemVariable.reg}
+}
+Else
+{
+    Rename-Item -Path $ExportDirPath\SystemVariable.reg -NewName $ExportDirPath\SystemVariable_$DateTime.reg
+    Write-Host "Exsiting file renamed..."
+    Invoke-Command {reg export $SystemVariablePath $ExportDirPath\SystemVariable.reg}
+}
+
+Write-Host "Exporting the User Variables..."
+
+If(!(Test-Path $ExportDirPath\UserVariable.reg))
+{
+    Invoke-Command {reg export $UserVariablePath $ExportDirPath\UserVariable.reg}
+}
+Else
+{
+    Rename-Item -Path $ExportDirPath\UserVariable.reg -NewName $ExportDirPath\UserVariable_$DateTime.reg
+    Write-Host "Exsiting file renamed..."
+    Invoke-Command {reg export $SystemVariablePath $ExportDirPath\UserVariable.reg}    
+}
+
+# Open the exported directory...
+Invoke-Item $ExportDirPath
---
+++ Environment Variable/ImportRegEnvVar.ps1
@@ -0,0 +1,12 @@
+﻿# Import registry of Environment Variables...
+
+$ImportDirPath = "C:\Imports"
+
+Write-Host "Importing the System Variables..."
+Invoke-Command {reg import $ImportDirPath\SystemVariable.reg}
+
+Write-Host "Importing the User Variables..."
+Invoke-Command {reg import $ImportDirPath\UserVariable.reg}
+
+# Open Environment Variables...
+rundll32 sysdm.cpl,EditEnvironmentVariables
