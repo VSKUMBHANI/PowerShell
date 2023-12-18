934f581c1b578c50462ab540de498745e5363d46
Vijay Kumbhani <102287173+VSKUMBHANI@users.noreply.github.com>
2023-12-18 10:32:10 +0530

    Update Set Volume.ps1

--- Set Volume.ps1
+++ Set Volume.ps1
@@ -1,1 +1,24 @@
+# https://github.com/VSKUMBHANI
+# Script is used to set volume.
 
+Function Set-Speaker($Volume)
+{
+    $wshShell = new-object -com wscript.shell;1..50 | 
+    % {$wshShell.SendKeys([char]174)};1..$Volume | 
+    % {$wshShell.SendKeys([char]175)}
+}
+
+##Sets volume to 20%
+Set-Speaker -Volume 10
+
+##Sets volume to 40%
+#Set-Speaker -Volume 20
+
+##Sets volume to 60%
+#Set-Speaker -Volume 30
+
+##Sets volume to 80%
+#Set-Speaker -Volume 40
+
+##Sets volume to 100%
+#Set-Speaker -Volume 50
