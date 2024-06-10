26ed8751e9856fdb1d1dd6b2f24a007aa86174cd
Vijay Patel <102287173+VSKUMBHANI@users.noreply.github.com>
2024-06-10 15:41:51 +0530

    Update CleanRecent.ps1

--- CleanRecent.ps1
+++ CleanRecent.ps1
@@ -10,3 +10,11 @@ Function Clear-RecentItems {
     Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackDocs"
 }
 Clear-RecentItems
+
+#To clean recent of excel
+Remove-Item "HKCU:\Software\Microsoft\Office\16.0\Excel\File MRU\"
+Remove-Item "HKCU:\Software\Microsoft\Office\16.0\Excel\Place MRU\"
+
+#To clean recent of word
+Remove-Item "HKCU:\Software\Microsoft\Office\16.0\Word\File MRU\"
+Remove-Item "HKCU:\Software\Microsoft\Office\16.0\Word\Place MRU\"
